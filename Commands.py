ad767dcde73de5e00a92b95a370d80ab5ea097c1
TunnelBlanket <TunnelBlanket@users.noreply.github.com>
2015-11-25 17:25:28 -0800

    Initial commit

---
+++ Commands.py
@@ -0,0 +1,189 @@
+from time import time
+
+class Commands(object):
+
+	# TODO: define "logger" property using self.bot's
+	def __init__(self, bot):
+		self.bot = bot
+
+		self.commands = {
+			"ping": self.handlePing,
+			"access": self.handleAccess,
+			"phraser": self.handlePhraser,
+			"join": self.handleJoin,
+			"leave": self.handleLeave
+		}
+
+		# Keeps track of the last time people have used commands
+		# Nickname as key, list containing count and timestamp as value
+		self.commandHistory = {}
+
+		self.bot.logger.info("Commands module initialized")
+
+	def handlePhraser(self, user, channel, args):
+		nickname = user.split("!")[0].lower()
+
+		if nickname not in self.bot.accessList:
+			self.bot.msg(nickname, "You are not authorized to perform that command.")
+
+		else:
+			try:
+				command = args[0].lower()
+				targetChannel = args[1]
+
+				try:
+					if command == "start":
+						self.bot.channelPhrasers[targetChannel].start(600)
+
+						self.bot.msg(nickname, "Phraser started for channel {0}".format(targetChannel))
+
+					elif command == "stop":
+						self.bot.channelPhrasers[targetChannel].stop()
+
+						self.bot.msg(nickname, "Phraser stopped for channel {0}".format(targetChannel))
+
+					else:
+						self.bot.msg(nickname, "Invalid command usage - start or stop.")
+
+				except KeyError:
+					self.bot.msg(nickname, "Phraser for channel {0} not found - remember that channel names are case sensitive."
+							.format(targetChannel))
+
+			except Exception as ex:
+				self.bot.msg(nickname, "Unknown error when handling phraser command - {0}".format(ex.message))
+
+	def handleAccess(self, user, channel, args):
+		nickname = user.split("!")[0].lower()
+
+		if nickname not in self.bot.accessList:
+			self.bot.msg(nickname, "You are not authorized to perform that command.")
+
+		else:
+			try:
+				command = args[0].lower()
+				originalNickname = args[1]
+				accessNickname = args[1].lower()
+
+				if command == "del":
+					try:
+						self.bot.accessList.remove(accessNickname)
+						self.bot.msg(nickname, "{0} was successfully removed from the access list.".format(originalNickname))
+
+					except ValueError as ex:
+						self.bot.msg(nickname, "That user doesn't exist within the access list.")
+
+				elif command == "add":
+					if accessNickname in self.bot.accessList:
+						self.bot.msg(nickname, "That user already has access.")
+
+					else:
+						self.bot.accessList.append(accessNickname)
+						self.bot.msg(nickname, "{0} was successfully added to the access list.".format(originalNickname))
+
+				else:
+					self.bot.msg(nickname, "Invalid command usage.")
+
+			except:
+				self.bot.msg(nickname, "Unable to execute command - maybe you're missing an arguemnt?")
+
+	def handleLeave(self, user, channel, args):
+		nickname = user.split("!")[0].lower()
+
+		if nickname in self.bot.accessList:
+			channelToLeave = args[0]
+			self.bot.leave(channelToLeave)
+
+	def handleJoin(self, user, channel, args):
+		nickname = user.split("!")[0].lower()
+
+		if nickname in self.bot.accessList:
+			channelToJoin = args[0]
+			self.bot.join(channelToJoin)
+
+	def handleRemove(self, user, channel, args):
+		nickname = user.split("!")[0].lower()
+
+		if nickname not in self.bot.accessList:
+			self.bot.msg(nickname, "You are not authorized to perform that command.")
+
+		else:
+			try:
+				command = args[0].lower()
+			
+				if command == "access":
+					accessNickname = args[1].lower()
+
+					self.bot.accessList.remove(accessNickname)
+
+				elif command == "phraser":
+					phraserChannel = args[1]
+
+					self.bot.channelPhrasers[phraserChannel].stop()
+
+			except IndexError:
+				self.bot.say(channel, "You're missing an argument (or several).")
+
+	def handlePing(self, user, channel, args):
+		self.bot.logger.debug("Handling PING command")
+
+		data = "".join(args)
+
+		self.bot.say(channel, "Pong {0}".format(data))
+
+	def allowCommand(self, nickname):
+		if nickname in self.commandHistory:
+			commandCount, lastCommandTime = self.commandHistory[nickname]
+
+			# If they've executed less than three commands within the past second
+			if commandCount < 3 and lastCommandTime <= time() - 1:
+				# Proceed
+				self.bot.logger.debug("Less than three commands within the past second")
+
+				return True
+
+			# If they've executed more than or equal to three commands
+			# within the past six seconds
+			elif commandCount >= 3 and lastCommandTime > time() - 6:
+				self.bot.logger.debug("Greater than or equal to three commands within the past six seconds")
+
+				return False
+
+			# They exceeded the limit, but it's been past six seconds
+			else:
+				self.bot.logger.debug("Command history count reset for {nickname}!", nickname=nickname)
+				self.commandHistory[nickname][0] = 0
+
+				return True
+
+		else:
+			return True
+
+	def recordCommand(self, nickname):
+		if nickname in self.commandHistory:
+			commandCount, lastCommandTime = self.commandHistory[nickname]
+		else:
+			commandCount = 0
+
+		commandCount += 1
+
+		self.commandHistory[nickname] = [commandCount, time()]
+
+	def handleCommand(self, user, channel, message):
+		self.bot.logger.debug("Handling command with message {message}", message=message)
+
+		tokens = message.split(" ")
+		command = tokens[0].lower()
+		args = tokens[1:]		
+
+		if command in self.commands:
+			# The reason this is defined here is because there isn't a point
+			# in defining it if we're not being commanded
+			nickname = user.split("!")[0].lower()
+
+			if self.allowCommand(nickname):
+				self.commands[command](user, channel, args)
+
+				self.recordCommand(nickname)
+
+		else:
+			self.bot.logger.info("Invalid command attempt: {command}", command=command)
---
+++ Discord.py
@@ -0,0 +1,202 @@
+#-*- coding: utf-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+import os
+
+from twisted.words.protocols.irc import IRCClient
+from twisted.internet.protocol import ClientFactory
+from twisted.internet import reactor, ssl
+from twisted.logger import Logger, textFileLogObserver
+from twisted.internet.task import LoopingCall
+from twisted.internet.threads import deferToThread
+
+from pymarkov import pymarkov
+
+import Commands
+
+class DiscordFactory(ClientFactory):
+
+	def __init__(self, accessList, ircAddress):
+		self.accessList = accessList
+
+		ircAddress, ircPort = ircAddress
+
+		if "+" in ircPort:
+			ircPort = int(ircPort[1:])
+
+			sslOptions = ssl.CertificateOptions()
+			reactor.connectSSL(ircAddress, ircPort, self, sslOptions)
+
+		else:
+			ircPort = int(ircPort)
+
+			reactor.connectTCP(ircAddress, ircPort, self)
+
+		reactor.run()
+
+	def buildProtocol(self, addr):
+		return Discord(self.accessList)
+
+class Discord(IRCClient):
+
+	nickname = "discord"
+	realname = "Discord"
+	username = "discord"
+	versionName = "Discord"
+	versionNum = "0.01"
+
+	magicFile = "true.txt"
+
+	def __init__(self, accessList):
+		self.logger = Logger(observer=textFileLogObserver(sys.stdout))
+
+		self.accessList = [nick.lower() for nick in accessList]
+
+		if not os.path.exists(self.magicFile):
+			self.logger.info("Creating magic file")
+
+			try:
+				with open(self.magicFile, "a"):
+					pass
+
+			except Exception as ex:
+				self.logger.error("Unable to create magic file! {0}".format(ex.message))
+				reactor.stop()
+
+		self.markovGenerator = pymarkov.MarkovChainGenerator(self.magicFile)
+
+		self.channels = []
+		self.channelPhrasers = {}
+
+		self.logger.debug("Discord initialized")
+
+		# Maybe add hook/plugin system here?
+
+		self.commands = Commands.Commands(self)		
+
+	def removeChannel(self, channel):
+		try:
+			self.channels.remove(channel)
+
+			self.channelPhrasers[channel].stop()
+			
+			del self.channelPhrasers[channel]
+
+		except:
+			self.logger.error("Error removing {channel} from collection", channel=channel)
+
+	def insertPhrase(self, phrase):
+		try:
+			with open(self.magicFile, "a") as magicFile:
+				magicFile.write("{0}\n".format(phrase))
+
+			try:
+				file, ext = os.path.splitext(self.magicFile)
+				os.remove("{0}-pickled{1}".format(file, ext))
+
+				self.markovGenerator = pymarkov.MarkovChainGenerator(self.magicFile)
+
+				# self.markovGenerator.populate_dict(self.magicFile)
+
+			except IOError as ex:
+				self.logger.error("Unable to delete pickled file. {0}".format(ex.message))			
+
+			# deferToThread(self.markovGenerator.populate_dict, self.magicFile).addCallback(lambda x: x)
+
+		except Exception as ex:
+			self.logger.error("Unable to insert phrase into magic file! {0}".format(ex.message))
+
+	def kickedFrom(self, channel, kicker, message):
+		self.removeChannel(channel)
+
+		self.logger.info("Kicked from {channel} by {kicker}", channel=channel, kicker=kicker)
+
+	def left(self, channel):
+		self.removeChannel(channel)
+
+		self.logger.info("Left {channel}", channel=channel)
+
+	def handleMessage(self, user, channel, message):
+		senderNickname = user.split("!")[0]
+
+		if message.startswith("~reload") and senderNickname in self.accessList:
+			self.logger.info("Reloading commands module")
+			self.say(channel, "Reloading.")
+
+			try:
+				commandsModule = reload(Commands)
+				self.commands = commandsModule.Commands(self)
+
+			except Exception as e:
+				self.say(channel, "Failed to load commands module - {0}".format(e.message))
+
+		elif message.startswith("~"):
+			# Don't log commands to the brain
+			commandMessage = message[1:]
+
+			self.commands.handleCommand(user, channel, commandMessage)
+
+		else:
+			self.logger.info("Adding {message!r} to brain", message=message)
+
+			# Avoid storing anything with the bot's name in it
+			brainMessage = message.strip(self.nickname)
+
+			self.insertPhrase(brainMessage)
+
+			try:
+				randomPhrase = self.markovGenerator.generate_sentence().strip(self.nickname)
+
+				print randomPhrase
+
+				if self.nickname in message and channel.startswith("#") and self.channelPhrasers[channel].running:
+					phrase = "{0}, {1}".format(senderNickname, randomPhrase)
+
+					self.say(channel, phrase)
+
+				elif channel == self.nickname:
+					self.logger.debug("Sending message to {nickname}", nickname=senderNickname)
+
+					self.msg(senderNickname, randomPhrase)
+
+				else:
+					pass
+
+			except IndexError as generationError:
+				self.logger.error(generationError.message)
+
+	def privmsg(self, user, channel, message):
+		self.logger.info("Received message from {user} in {channel}", user=user, channel=channel)
+
+		# deferToThread(self.handleMessage, user, channel, message)
+		self.handleMessage(user, channel, message)
+
+	def signedOn(self):
+		self.logger.info("Signed on")
+
+		self.join("#bots")
+
+	def joined(self, channel):
+		self.channels.append(channel)
+
+		self.logger.info("Joined channel {channel!r}", channel=channel)
+
+		channelPhraser = LoopingCall(self.sayRandomPhrase, channel)
+		reactor.callLater(2, channelPhraser.start, 600)
+
+		self.channelPhrasers[channel] = channelPhraser
+
+	def sayRandomPhrase(self, channel):
+		try:
+			randomPhrase = self.markovGenerator.generate_sentence()
+			self.say(channel, randomPhrase)
+
+		except IndexError:
+			pass  # Phrases aren't ready yet
+
+
+scriptFile, ircAddress, ircPort = sys.argv
+
+DiscordFactory(["arthur"], (ircAddress, ircPort))
